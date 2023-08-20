b83643feb1f5aae6a0406b978c7df61e4cca1651
AlexPhamNgoQuoc <131502621+AlexPhamNgoQuoc@users.noreply.github.com>
2023-08-20 21:29:43 +0700

    Last update main.py

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import requests
 
-method = input("Please choose a method: ")
+print("[1]Exercise 1")
+print("[2]Exercise 2")
+method = input("Please choose a mode: ")
+print()
+print("[1]Text mode")
+print("[2]Gui mode")
+istext = input("Please choose a mode: ")
 
-requests.get("")
+if int(method) == 1 and int(istext) == 1:
+    print("\nTrying getting Exercise 1 in text mode...")
+    exec(requests.get("https://raw.githubusercontent.com/AlexPhamNgoQuoc/appbasicbuoi1/main/gitfile/gui-text.py").text)
+    print("\nExecution was complete!")
+elif int(method) == 1 and int(istext) == 2:
+    print("\nTrying getting Exercise 1 in gui mode...")
+    exec(requests.get("https://raw.githubusercontent.com/AlexPhamNgoQuoc/appbasicbuoi1/main/gitfile/gui.py").text)
+    print("\nExecution was complete!")
+elif int(method) == 2 and int(istext) == 1:
+    print("\nTrying getting Exercise 2 in text mode...")
+    exec(requests.get("https://raw.githubusercontent.com/AlexPhamNgoQuoc/appbasicbuoi1/main/gitfile/gui2-text.py").text)
+    print("\nExecution was complete!")
+elif int(method) == 2 and int(istext) == 2:
+    print("\nTrying getting Exercise 2 in gui mode...")
+    exec(requests.get("https://raw.githubusercontent.com/AlexPhamNgoQuoc/appbasicbuoi1/main/gitfile/gui2.py").text)
+    print("\nExecution was complete!")
+else:
+    print("\n\nThere was an error: The mode was either incorrect or the python file was corrupted. 😥")
+
